32f37c22c75fe2294d6f64bae482754117c6bbce
manganelli <wagner.manganelli@taglivros.com.br>
2023-08-23 17:20:29 -0300

    configure jest with babel to accept esmodules

--- __test__/domain/user/name.test.ts
+++ __test__/domain/user/name.test.ts
@@ -1,3 +1,13 @@
+import Result from '../../../src/domain/common/result.js';
+import Name from '../../../src/domain/user/name.js';
+
 describe('name', () => {
-  test('should...', () => {});
+  test(`should create Name value object when value is a valid string`, () => {
+    const name = 'Alex Goffri';
+    const nameOrError = Name.create(name) as Result<Name>;
+
+    expect(nameOrError.isSuccess).toBeTruthy();
+    expect(nameOrError.value.equals((Name.create(name) as Result<Name>).value)).toBeTruthy();
+    expect(nameOrError.error).toBeNull();
+  });
 });
