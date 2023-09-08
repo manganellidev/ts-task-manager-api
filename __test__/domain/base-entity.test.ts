d7d2235fbc1399b186c098fe3c9105719bb11d3f
manganelli <wagner.manganelli@taglivros.com.br>
2023-09-08 16:28:11 -0300

    remove unused method equals from base entity class

--- __test__/domain/base-entity.test.ts
+++
@@ -1,3 +0,0 @@
-describe('base-entity', () => {
-  test('should...', () => {});
-});
--- src/domain/base-entity.ts
+++ src/domain/base-entity.ts
@@ -4,16 +4,12 @@ export interface IBaseEntity {
   id: string;
 }
 
-export default class BaseEntity implements IBaseEntity {
+export default abstract class BaseEntity implements IBaseEntity {
   constructor(protected readonly _id: string) {
     this._id = _id ? _id : randomUUID();
   }
 
   get id() {
     return this._id;
-  }
-
-  equals(other: { id: string }) {
-    return this.id === other.id;
   }
 }
